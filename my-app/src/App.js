7f8901c3bda9842635e8526ffb50ccb34d17c685
Gurpreet Dhaliwal <gdhaliwal22@gmail.com>
2019-11-18 19:34:34 -0800

    updated components in App FormikLogin and FormikRegister

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,17 +1,18 @@
 import React from "react";
+import PrivateRoute from "./utils/PrivateRoute";
 import "./App.css";
-import { Route } from "react-router-dom";
+import { Switch, BrowserRouter as Router, Route, Redirect, } from "react-router-dom";
 
-import Login from "./Components/Login";
-import Register from "./Components/Register";
+import FormikLogin from "./Components/Login";
+import FormikRegister from "./Components/Register";
 import Restaurant from "./Components/Restaurant";
 import Review from "./Components/Review";
 
 function App() {
   return (
     <div className="App">
-      <Route exact path="/" component={Login} />
-      <Route path="/register" component={Register} />
+      <Route exact path="/" component={FormikLogin} />
+      <Route path="/register" component={FormikRegister} />
       <Route path="/rest" component={Restaurant} />
       <Route path="/review" component={Review}/>
     </div>
