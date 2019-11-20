6beb2a51c9f14c795f5ac6674f21b9bbd8d6ac5e
blakelower <blakelower1999@gmail.com>
2019-11-20 15:50:22 +0700

    changes

--- my-app/src/Components/Register.js
+++ my-app/src/Components/Register.js
@@ -1,5 +1,4 @@
 import React, { useState } from "react";
-import { Link } from "react-router-dom";
 import { Form, Field, withFormik } from "formik";
 import * as Yup from "yup";
 import { connect } from "react-redux";
